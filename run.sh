de0ccce10c0009a46d536411a55ba3365e6acb88
Peter Jaric <peter.jaric@uadm.uu.se>
2025-02-21 16:46:36 +0100

    Check if spreadsheet exists before dowloading.

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-curl -L https://docs.google.com/spreadsheets/d/1lsB-p2w0te_Ui4QYDLp49wB-_7vYrkSPVCt4fhkjz-I/export\?format\=xlsx -o WordFeud.xlsx
+
+FILENAME="WordFeud.xlsx"
+
+# Check if file exists before downloading
+if [ -f "$FILENAME" ]; then
+  echo "File exists, skipping download. Remove $FILENAME to redownload."
+else
+  echo "File does not exist, downloading"
+  curl -L https://docs.google.com/spreadsheets/d/1lsB-p2w0te_Ui4QYDLp49wB-_7vYrkSPVCt4fhkjz-I/export\?format\=xlsx -o "$FILENAME"
+fi
 python convert_to_matches.py
 rayter wordfeudligan.txt
